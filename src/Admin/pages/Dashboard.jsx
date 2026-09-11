c7ee2a171980638d171ae4d71654ede03f237bba
Anirudh <iamanirudh2902@gmail.com>
2026-09-11 19:57:13 +0530

    feat: update admin dashboard

--- src/Admin/pages/Dashboard.jsx
+++ src/Admin/pages/Dashboard.jsx
@@ -1,15 +1,24 @@
-import React from 'react'
-import AdminHeader from '../components/AdminHeader'
-import Footer from '../../User/components/Footer'
+import React from "react";
+import AdminHeader from "../components/AdminHeader";
+import AdminSidebar from "../components/AdminSidebar";
+import Footer from "../../User/components/Footer";
 
 function Dashboard() {
   return (
     <>
-    <AdminHeader/>
-    {/* DASHBOARD */}
-    <Footer/>
+      <AdminHeader />
+      {/* DASHBOARD */}
+      <div className="w-screen">
+        <div className="w-full h-full grid sm:grid-cols-1 md:grid-cols-12">
+          <div className="col-span-3">
+            <AdminSidebar />
+          </div>
+          <div className="col-span-9">Dashboard</div>
+        </div>
+      </div>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;

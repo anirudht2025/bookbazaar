fc772cf73cff81c215fcc5e336d1e97a67dfba15
Anirudh <iamanirudh2902@gmail.com>
2026-09-11 19:57:34 +0530

    feat: update admin settings

--- src/Admin/pages/Settings.jsx
+++ src/Admin/pages/Settings.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import AdminHeader from "../components/AdminHeader";
+import Footer from "../../User/components/Footer";
+import AdminSidebar from "../components/AdminSidebar";
 
 function Settings() {
   return (
-    <div>Settings</div>
-  )
+    <>
+      <AdminHeader />
+      {/* Dashboard */}
+      <div>
+        <div className="w-screen">
+          <div className="h-full grid grid-cols-1 md:grid-cols-12">
+            <div className="md:col-span-3">
+              <AdminSidebar />
+            </div>
+            <div className="md:col-span-9">
+              <h1 className="text-3xl">Settings</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
 }
 
-export default Settings
+export default Settings;

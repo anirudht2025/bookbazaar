d363b0ba2c8b781e54758f1208e6d4de92adc751
Anirudh <iamanirudh2902@gmail.com>
2026-09-14 17:12:14 +0530

    feat: update admin settings

--- src/Admin/pages/Dashboard.jsx
+++ src/Admin/pages/Dashboard.jsx
@@ -7,15 +7,40 @@ function Dashboard() {
   return (
     <>
       <AdminHeader />
+
       {/* DASHBOARD */}
       <div className="w-screen">
-        <div className="w-full h-full grid sm:grid-cols-1 md:grid-cols-12">
-          <div className="col-span-3">
+        <div className="grid h-full grid-cols-1 md:grid-cols-12">
+          {/* Sidebar */}
+          <div className="md:col-span-3">
             <AdminSidebar />
           </div>
-          <div className="col-span-9">Dashboard</div>
+
+          {/* Dashboard Content */}
+          <div className="p-3 md:col-span-9 me-5">
+            <div className="m-2 grid grid-cols-1 gap-2 md:grid-cols-3">
+              {/* Books */}
+              <div className="rounded-lg bg-violet-500 p-10 text-lg text-white">
+                <p>Total Number of Books</p>
+                <h2 className="mt-2 text-3xl font-bold">100+</h2>
+              </div>
+
+              {/* Users */}
+              <div className="rounded-lg bg-green-500 p-10 text-lg text-white">
+                <p>Total Number of Users</p>
+                <h2 className="mt-2 text-3xl font-bold">100+</h2>
+              </div>
+
+              {/* Employees */}
+              <div className="rounded-lg bg-orange-500 p-10 text-lg text-white">
+                <p>Total Number of Employees</p>
+                <h2 className="mt-2 text-3xl font-bold">20+</h2>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
+
       <Footer />
     </>
   );
